782af8f1e2fca37630002656c8e98dae9db7faed
Wellington Silva da Mota <silvaemota@gmail.com>
2018-11-05 10:49:46 -0200

    Incluído chamada do SplashScreen e instalado pacote

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 export default class App extends Component {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+
   render() {
     return (
       <View style={styles.container}>
